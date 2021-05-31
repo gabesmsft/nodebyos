2809f28ecf37306cbf2419698f838f4dc1abc6d8
Gabe Shapiro <gabeshap@outlook.com>
2021-05-31 03:35:45 -0700

    added reference to static image that will be served as part of site

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/MySiteStaticContents', (req, res) => {
-  res.write(`Static file 1: <hr/><img src="/public/banner1.jpg}" width="500">`);
-  res.write(`Static file 2: <hr/><img src="/public/banner2.jpg}" width="500">`);
+  res.write(`Static file 1: <hr/><img src="/public/banner1.jpg" width="500">`);
+  res.write(`Static file 2: <hr/><img src="/public/banner2.jpg" width="500">`);
   res.end();
 });
 
